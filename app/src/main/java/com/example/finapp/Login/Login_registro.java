9f29c48fdb1287a334a8feea90d1030092991967
SamuelPerez27 <starlingsamuel_bhdleon.com.do>
2021-11-26 18:28:57 -0400

    upload

---
+++ app/src/main/java/com/example/finapp/Login/Login_registro.java
@@ -0,0 +1,16 @@
+package com.example.finapp.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finapp.R;
+
+public class Login_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_registro);
+    }
+}
