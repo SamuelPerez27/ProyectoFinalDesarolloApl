b3db2481756de678cb8c27852b44a859ee6be52a
Samuel Perez <61362199+SamuelPerez27@users.noreply.github.com>
2021-11-27 19:57:10 -0400

    Update total_cliente.php

--- cliente/total_cliente.php
+++ cliente/total_cliente.php
@@ -1,23 +1,25 @@
 <?php
 
-$conexion =mysqli_connect("localhost", "id18004791_admin", "3lLid3rPili#73","id18004791_teorganizo");
+$conexion =mysqli_connect("localhost", "id18017212_id18004791_admin", "3lLid3rPili#73","id18017212_id18004791_teorganizo");
 if(!$conexion){
     echo "Error de conexion";
 }
+
 $result= array();
-$result['datos'] =array();
-$query ="SELECT * FROM select sum(Id_Cliente) as 'total' from cliente";
+$result['cliente'] =array();
+$query ="select sum(Id_Cliente) as 'total' from cliente";
 $response = mysqli_query($conexion, $query);
 
 while($row = mysqli_fetch_array($response))
 {
   $index['total'] =$row['0'];
 
-  array_push($result['datos'],$index);
+
+  array_push($result['cliente'],$index);
 }
 $result["Exito"]="1";
-
 echo json_encode($result);
 
-mysqli_close($conexion);
+
+
 ?>
