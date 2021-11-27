bc251bd552042999ce2577f9c4fed2f733762bf6
Samuel Perez <61362199+SamuelPerez27@users.noreply.github.com>
2021-11-27 19:47:33 -0400

    Add files via upload

---
+++ cliente/total_cliente.php
@@ -0,0 +1,23 @@
+<?php
+
+$conexion =mysqli_connect("localhost", "id18004791_admin", "3lLid3rPili#73","id18004791_teorganizo");
+if(!$conexion){
+    echo "Error de conexion";
+}
+$result= array();
+$result['datos'] =array();
+$query ="SELECT * FROM select sum(Id_Cliente) as 'total' from cliente";
+$response = mysqli_query($conexion, $query);
+
+while($row = mysqli_fetch_array($response))
+{
+  $index['total'] =$row['0'];
+
+  array_push($result['datos'],$index);
+}
+$result["Exito"]="1";
+
+echo json_encode($result);
+
+mysqli_close($conexion);
+?>
